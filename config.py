16b2348080e97195c58be3c0e831bf98a5d092b1
jcortenbach <37661049+jcortenbach@users.noreply.github.com>
2025-01-21 14:26:07 +0100

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ PRINTER_ID = os.getenv('PRINTER_ID')  # Printer serial number - Run init_bambula
 PRINTER_CODE = os.getenv('PRINTER_ACCESS_CODE')       # Printer access code - Run init_bambulab.py
 PRINTER_IP = os.getenv('PRINTER_IP')     # Printer local IP address - Check wireless on printer
 SPOOLMAN_BASE_URL = os.getenv('SPOOLMAN_BASE_URL')
-SPOOLMAN_API_URL = f"{SPOOLMAN_BASE_URL}/api/v1"
+SPOOLMAN_API_URL = "192.168.1.7:7912/api/v1"
 AUTO_SPEND = os.getenv('AUTO_SPEND', False)
